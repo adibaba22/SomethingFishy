94156b60aace998cd870223ea71c38f38084450a
ashikka <ashikagupta28@gmail.com>
2020-07-06 17:34:30 +0530

    feat: Removing oauth

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const passport = require('passport');
-const cookieSession = require('cookie-session');
-require('./middleware/passportSetup');
 
 
 const userRoutes = require('./routes/user');
@@ -25,13 +22,7 @@ app.use(express.static('uploads'))
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use(cookieSession({
-    name: 'session',
-    keys: ['key1', 'key2']
-}))
 
-app.use(passport.initialize())
-app.use(passport.session())
 
 app.use("/user", userRoutes);
 app.use("/products", productRoutes);
@@ -44,13 +35,7 @@ app.get('/success', (req, res) => {
 })
 
 
-app.get('/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
 
-app.get('/google/callback', passport.authenticate('google', {failureRedirect: '/failed'}), function (req, res) { // Successful authentication, redirect home.
-    res.redirect('/success');
-});
 
 app.listen(process.env.PORT, () => {
     console.log("Server up and running on port " + process.env.PORT);
